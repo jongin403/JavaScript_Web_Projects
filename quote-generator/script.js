71ff15bb9f746ac70f6ee0bc3d5b43ff8e31fe08
jongin.jeong <jongin403@naver.com>
2022-02-19 13:16:32 +0900

    Feat: 명언 선택 및 버튼 이벤트 추가
    랜덤 명언 선택
    다른 명언 찾기
    트위터 공유하기

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,10 +1,46 @@
-// Get Quotes From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    if(quote.author) authorText.textContent = quote.author;
+    else authorText.textContent = 'Unknown';
+
+    quoteText.textContent = quote.text;
+
+    if(120 < quote.text.length) quoteText.classList.add('long-quote');
+    else quoteText.classList.remove('long-quote');
+}
+
+// Get Quotes From API
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
-
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
     } catch (error) {
-        //
+        // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load();
+getQuotes();
